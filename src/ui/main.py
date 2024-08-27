23a3d5e3f1231ec53b66d8256f403ac1b67da6e0
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2024-08-27 16:51:31 -0500

    ui controls

--- src/ui/main.py
+++ src/ui/main.py
@@ -1,10 +1,13 @@
 from pathlib import Path
 
+import duckdb
 import streamlit as st
+
+con = duckdb.connect(database=':memory:')
 
 history = st.radio("Reading history", ["Full", "Filtered"])
 
-newsletter = st.radio("Newsletter", sorted(list(Path('./data').rglob('*.md'))))
+newsletter = st.radio("Newsletter", sorted(list(Path('./data').rglob('*.csv'))))
 
 if history == "Full":
     with open("data/reading_history.txt", "r") as f:
@@ -12,4 +15,12 @@ if history == "Full":
 else:
     with open("data/reading_history_filtered.txt", "r") as f:
         history = f.read()
+
+newsletter_items = con.execute(f"SELECT * FROM '{newsletter}' WHERE headline != 'SKIP'").fetch_df()
+
+
+with st.expander("See reading history"):
+    st.code(history)
+
+st.dataframe(newsletter_items)
 
