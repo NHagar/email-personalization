0b02629d7e89526930f92925e0e5f21b500a841b
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2025-02-12 21:46:02 -0600

    interface sketch

--- ui_survey/prompt_iterator.py
+++ ui_survey/prompt_iterator.py
@@ -1,5 +1,6 @@
 import duckdb
 import streamlit as st
+from headline_generation import HeadlineGenerator, newsletter_paths
 
 con = duckdb.connect("database.db")
 
@@ -19,3 +20,48 @@ headlines_selected = st.multiselect(
     "Select headlines for reading history",
     headlines,
 )
+
+generator = HeadlineGenerator(headlines_selected)
+
+prompt_summary = st.text_area(
+    "User interest summary prompt", value=generator.prompt_history, height=250
+)
+
+prompt_ranking = st.text_area(
+    "Headline ranking prompt",
+    value=generator.prompt_ranking,
+    height=250,
+)
+
+prompt_headline_writing = st.text_area(
+    "Headline writing prompt",
+    value=generator.prompt_framing,
+    height=250,
+)
+
+newsletter_selected = st.selectbox(
+    "Select newsletter edition",
+    newsletter_paths,
+)
+
+generator.load_newsletter(newsletter_selected)
+
+st.write("Newsletter items:")
+st.write(generator.newsletter_items)
+
+steps = st.pills(
+    "Select the steps to run",
+    ["Infer interests", "Rank items", "Write headlines"],
+    selection_mode="multi",
+)
+
+if st.button("Generate outputs"):
+    if "Infer interests" in steps:
+        generator.infer_interests()
+        st.write("User interests inferred")
+    if "Rank items" in steps:
+        generator.rank_items()
+        st.write("Items ranked")
+    if "Write headlines" in steps:
+        generator.write_headlines()
+        st.write("Headlines written")
